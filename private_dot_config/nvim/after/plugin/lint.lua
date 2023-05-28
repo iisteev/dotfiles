7fbf5cb9ad3757d669c927e0e2f31e21d2d8b540
iisteev <isteevan.shetoo@is-info.fr>
2023-05-28 12:16:48 +0200

    Use default golangcilint linter config

--- private_dot_config/nvim/after/plugin/lint.lua
+++ private_dot_config/nvim/after/plugin/lint.lua
@@ -1,18 +1,8 @@
 local lint = require('lint')
-local golangcilint = require("lint.linters.golangcilint")
 
 lint.linters_by_ft = {
     python = {'flake8'},
     go = {"golangcilint"},
-}
-
-golangcilint.append_fname = true
-golangcilint.args = {
-    "run",
-    "--out-format",
-    "json",
-    "--timeout",
-    "5m",
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {

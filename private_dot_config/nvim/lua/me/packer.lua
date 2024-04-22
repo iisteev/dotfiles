972e49f86e2f2e2f8f084d93f9917a13462682c1
iisteev <isteevan.shetoo@is-info.fr>
2024-04-22 20:20:01 +0200

    bump telescope to 0.1.6

--- private_dot_config/nvim/lua/me/packer.lua
+++ private_dot_config/nvim/lua/me/packer.lua
@@ -16,7 +16,7 @@ return require('packer').startup(function(use)
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use {

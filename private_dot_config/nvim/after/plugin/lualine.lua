c427a2171519425f8ce07ca65812b3bb07bc84ca
iisteev <isteevan.shetoo@is-info.fr>
2026-07-11 16:08:20 +0200

    Migrate to lazyvim

--- private_dot_config/nvim/after/plugin/lualine.lua
+++
@@ -1,9 +0,0 @@
-local lua_line = require("lualine")
-
-lua_line.setup {
-	options = {
-		theme = 'onedark',
-		component_separators = '|',
-		section_separators = '',
-	}
-}
---
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"folke/snacks.nvim",
+		init = function()
+			vim.g.snacks_animate = false
+		end,
+		opts = {},
+	},
+}
